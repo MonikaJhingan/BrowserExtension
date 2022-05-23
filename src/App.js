a8fea84ed56381090d5b7e58f817b4a22aa2b814
MonikaJhingan <67049170+MonikaJhingan@users.noreply.github.com>
2022-05-23 13:17:59 +0530

    homepage, useronboarding added

--- src/App.js
+++ src/App.js
@@ -1,41 +1,33 @@
-import "./App.css";
-import logo from "./logo.png";
+import React, { useEffect, useState } from "react";
+import { useBrowser } from "./contexts";
+import { pics } from "./database";
+import { HomePage, UserOnboarding } from "./pages";
 
 function App() {
+  const {
+    browserState: { userName }, browserDispatch
+  } = useBrowser();
+  const [bgImage, setBgImage] = useState("");
+
+  useEffect(() => {
+    const getBgImage = pics[Math.floor(Math.random() * pics.length)];
+    setBgImage(getBgImage);
+    const getName=localStorage.getItem("userName");
+    browserDispatch({
+      type: "USERNAME",
+      payload: getName,
+    });
+    // eslint-disable-next-line
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} alt="mockBee logo" width="180" height="180" />
-        <h1 className="brand-title">
-          Welcome to <span>mockBee!</span>
-        </h1>
-        <p className="brand-description">
-          Get started by editing <code>src/App.js</code>
-        </p>
-        <div className="links">
-          <a
-            href="https://mockbee.netlify.app/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Explore mockBee
-          </a>
-          <a
-            href="https://mockbee.netlify.app/docs/api/introduction"
-            target="_blank"
-            rel="noreferrer"
-          >
-            API Documentation
-          </a>
-          <a
-            href="https://github.com/neogcamp/mockBee"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Contribute
-          </a>
-        </div>
-      </header>
+    <div
+      className="h-screen bg-center bg-no-repeat bg-cover"
+      style={{
+        backgroundImage: `url("${bgImage}")`,
+      }}
+    >
+      {userName ? <HomePage /> : <UserOnboarding />}
     </div>
   );
 }
